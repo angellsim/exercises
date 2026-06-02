183f45ff53bf9eac7754c8d4fbe81101a95ee3f3
raiden <alicewolffernandes@gmail.com>
2026-06-01 23:16:52 -0300

    lógica corrigida e encontrada porém refinar

--- src/SeqBemFormada/Main.java
+++ src/SeqBemFormada/Main.java
@@ -1,5 +1,7 @@
+// Aqui adicionei o package por conta de estar localizado em uma pasta cheio de outros exercícios diferentes
 package SeqBemFormada;
 
+// Importei Scanner e ArrayList pq foram funções que eu utilizei (pra ler input do user e criar lista)
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -8,13 +10,15 @@ public class Main{
   {
     Scanner scanner = new Scanner(System.in);
 
+    // Criei uma lista pra ir adicionando o que o usuário fosse digitando
     ArrayList<String> valorDigitado1 = new ArrayList<String>();
 
+    // Criei uma "str" vazia no formato de string q eu usaria um método de join nela depois pra pegar a string completa da lista
     String str = "";
 
     for (int i = 0; i < 100; i++){
 
-      System.out.println("Digite: ");
+      System.out.print("Digite: ");
 
       String valorDigitado2 = scanner.nextLine();
 
@@ -27,9 +31,29 @@ public class Main{
       str = String.join("", valorDigitado1);      
     }
 
-    System.out.println(valorDigitado1);
-    System.out.println(str);
+    int contPar1 = 0;
+    int contPar2 = 0;
 
+    int contCol1 = 0;
+    int contCol2 = 0;
+
+    for (int i = 0; i < str.length(); i++){
+
+      if (i == 0 && valorDigitado1.get(i).equals(")") || i == 0 && valorDigitado1.get(i).equals("]")){
+        System.out.println("MALFORMADA");
+        break;
+      } 
+
+      if (valorDigitado1.get(i).equals("(")){   contPar1++;  }
+      else if (valorDigitado1.get(i).equals(")")){   contPar2++;  }
+      else if (valorDigitado1.get(i).equals("[")){   contCol1++;  }
+      else if (valorDigitado1.get(i).equals("]")){   contCol2++;  }
+
+      if (contPar2 > contPar1 || contCol2 > contCol1){
+        System.out.println("MALFORMADA");
+        break;
+      }
+    }
     scanner.close();
   }
 }
