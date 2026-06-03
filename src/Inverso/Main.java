606841a59cad6dd6c9a7d12f1c06406cc536feb1
raiden <alicewolffernandes@gmail.com>
2026-06-02 23:15:30 -0300

    add: exercicio de numero inverso

---
+++ src/Inverso/Main.java
@@ -0,0 +1,28 @@
+package Inverso;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+
+    // Scanner para ler oq o usuário digita
+    Scanner scanner = new Scanner(System.in);
+
+    // Passo para ler oq o usuário digitou
+    System.out.print("Digite um número com no máximo 3 dígitos: ");
+    int valorDigitado = scanner.nextInt();
+
+    // Lógica usada para pegar cada dígito individualmente
+    String primeiroDigito = String.valueOf(valorDigitado/100);
+    String segundoDigito = String.valueOf((valorDigitado/10) % 10);
+    String terceiroDigito = String.valueOf(valorDigitado%10);
+  
+    // Variável que irá conter o número inteiro invertido
+    String valorInverso = terceiroDigito + segundoDigito + primeiroDigito;
+
+    System.out.println(valorInverso);
+
+    scanner.close();
+
+  }
+}
